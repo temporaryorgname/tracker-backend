16e12326d83337a477166a8c29197d5737046d0a
Howard <howard.huang@mail.mcgill.ca>
2019-03-25 20:08:29 +0000

    DB update script

--- database/0.4-to-0.5.sql
+++ database/0.4-to-0.5.sql
@@ -10,12 +10,12 @@ IF EXISTS (select * from meta where key='SCHEMA_VERSION' AND value='0.4') THEN
 UPDATE public.meta SET value='0.5' WHERE key='SCHEMA_VERSION';
 
 ALTER TABLE public.photo ADD "food_id" integer;
-UPDATE public.photo p SET food_id=(SELECT id FROM public.food WHERE photo_id=p.id);
-UPDATE public.photo p SET food_id=(SELECT id FROM public.food WHERE photo_group_id=p.group_id AND parent_id IS NULL);
+UPDATE public.photo p SET food_id=(SELECT id FROM public.food WHERE photo_id=p.id OR (photo_group_id=p.group_id AND parent_id IS NULL));
 
-ALTER TABLE public.food DROP COLUMN photo_id;
-ALTER TABLE public.food DROP COLUMN photo_group_id;
-ALTER TABLE public.photo DROP COLUMN group_id;
+-- TODO on next iteration. Can't delete them on the same transaction block as the stuff above.
+--ALTER TABLE public.food DROP COLUMN photo_id;
+--ALTER TABLE public.food DROP COLUMN photo_group_id;
+--ALTER TABLE public.photo DROP COLUMN group_id;
 
 ----------------------------------------------------------------------------------------------------
 --- Update script ends here
