3983ce914ef9d151ddf97d37a0c8df52337152c0
Howard <howard.huang@mail.mcgill.ca>
2019-11-19 19:28:14 +0000

    Add auth endpoints to Swagger

--- fitnessapp/resources/auth.py
+++ fitnessapp/resources/auth.py
@@ -35,6 +35,35 @@ def request_loader(request):
 
 @auth_bp.route('/login', methods=['POST'])
 def login():
+    """ Log in.
+    ---
+    tags:
+      - auth
+    parameters:
+      - in: body
+        name: body
+        required: true
+        schema:
+          type: object
+          properties:
+            email:
+              type: string
+              example: 'name@email.com'
+            password:
+              type: string
+            permanent:
+              type: boolean
+    responses:
+      200:
+        schema:
+          type: number
+      403:
+        schema:
+          type: object
+          properties:
+            error:
+              type: string
+    """
     data = request.get_json()
     email = data['email']
     session.permanent = False
@@ -53,6 +82,15 @@ def login():
 
 @auth_bp.route('/current_session', methods=['GET'])
 def get_current_session():
+    """ Log in.
+    ---
+    tags:
+      - auth
+    responses:
+      200:
+        schema:
+          type: object
+    """
     try:
         print(current_user)
         if current_user.get_id() is not None:
@@ -64,5 +102,14 @@ def get_current_session():
 @auth_bp.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
+    """ Log out.
+    ---
+    tags:
+      - auth
+    responses:
+      200:
+        schema:
+          type: object
+    """
     flask_login.logout_user()
     return '{}', 200
